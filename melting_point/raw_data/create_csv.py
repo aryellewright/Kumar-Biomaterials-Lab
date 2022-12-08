0aae94773f99f6279ccca5e786ace30a6deb32e0
camdenfritz <camden.fritz123@gmail.com>
2022-12-08 11:53:38 -0700

    Add instructions to create_csv

--- melting_point/raw_data/create_csv.py
+++ melting_point/raw_data/create_csv.py
@@ -1,8 +1,18 @@
+"""
+This function will reformat a csv that has space delimiter instead of comma.
 
-with open('./20221105_N100H100_DSF.csv', 'r') as f:
-    with open('./20221105_N100H100_DSF_new.csv', 'w+') as new_file:
-        for i in f.readlines():
-            new_line = i.split()
-            new_line = ','.join(new_line)
-            # print(new_line)
-            new_file.write(new_line + '\n')
+:params:[Required] filename: Name of csv file to be read in
+:params:[Optional] new_file_postfix: Postfix of new file to not have duplicate.
+                                     default='_new'
+"""
+
+
+def remove_spaces(filename: str, new_file_postfix: str = '_new'):
+    with open(filename, 'r') as f:
+        new_filename = filename.split('.')
+        new_filename = f'{new_filename[0]}{new_file_postfix}.csv'
+        with open(new_filename, 'w+') as new_file:
+            for i in f.readlines():
+                new_line = i.split()
+                new_line = ','.join(new_line)
+                new_file.write(new_line + '\n')
